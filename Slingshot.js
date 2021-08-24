bbe19c383ffef1a772ca830768488328ab26ed7f
aparna-wht <72865533+aparna-wht@users.noreply.github.com>
2021-08-24 16:21:46 +0530

    Add files via upload

---
+++ Slingshot.js
@@ -0,0 +1,24 @@
+class Slingshot{
+    constructor(bodyA,pointB){
+        var options = {
+            bodyA:bodyA,
+            pointB:pointB,
+            stiffness:0.04,
+            length:10
+        }
+
+        this.slingshot = Constraint.create(options);
+        this.pointB = pointB;
+        World.add(world,this.slingshot);
+    }
+
+    display(){
+        var posA = this.slingshot.bodyA.position;
+        var posB = this.pointB;
+
+        push();
+        strokeWeight(3);
+        line(posA.x,posA.y,posB.x,posB.y);
+        pop();
+    }
+}
